5bb45923f2875048d0505504ff601859d177c10b
gsanchez13 <gisellesanchez@pursuit.org>
2021-01-08 14:28:03 -0500

    creates another answer for tallestCandleCount

--- hackerrank/tallestCandleCount.js
+++ hackerrank/tallestCandleCount.js
@@ -15,3 +15,14 @@ function birthdayCakeCandles(ar) {
     }
     return tallest
     }
+
+    // using js and array functions, this function is able to find the largest number from a set of numbers, then create a new, filtered array that has the criteria of finding elements that match the largest number
+    // after, the length of the filtered array is returned
+    // O^n time complexity
+    // O^n sace complexity
+    
+    const birthdayCakeCandles = (arr) => {
+        let tallest = Math.max(...arr)
+        let onlyTallestArr = arr.filter(i => i === tallest)
+        return onlyTallestArr.length
+    }
