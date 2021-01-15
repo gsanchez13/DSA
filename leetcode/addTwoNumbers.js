e28c921dfb18e3caae5a22ba0d06dd6925c65cb7
gsanchez13 <gisellesanchez@pursuit.org>
2021-01-14 23:15:18 -0500

    fully solves probem with edge cases & comments, On time and space

--- leetcode/addTwoNumbers.js
+++ leetcode/addTwoNumbers.js
@@ -19,22 +19,25 @@ const addTwoNumbers = (l1, l2) => {
         }
         return arr
     };
-
+// creates a helper function that can create an array out of a linked list head node
     let l1Arr = getNodeVals(l1, []);
     let l2Arr = getNodeVals(l2, []);
-
+// creates the associated arrays from the linked list nodes in arguments
     const getBackwardsNum = (arr) => {
         let num = ''
         for (let i = arr.length - 1; i >= 0; i--) {
             num += arr[i]
         }
-        return BigInt(Number(num))
+        return BigInt(num)
     };
+// creates the value of the concatinated number of the array backwards by concatinating it to a string, then converting it into a BigInt aka number
+// Due to JavaScript limitations, in order to avoid exponentials, BigInt() can be used to display all integers
+// However, BigInt() cannot be used with Number() as it can cause inaccuracy in numerical value
 
     let l1Num = getBackwardsNum(l1Arr);
     let l2Num = getBackwardsNum(l2Arr);
-    let sumOfLL = BigInt(l1Num + l2Num);
-
+    let sumOfLL = l1Num + l2Num;
+// gets the sum of the linked lists backwards
     const createBackwardsLL = (num) => {
         let numArr = num.toString().split("");
         let headNode = new ListNode(Number(numArr[numArr.length - 1]))
@@ -48,6 +51,7 @@ const addTwoNumbers = (l1, l2) => {
         }
         return headNode
     };
-
+// creates a helper function that takes in that number, creates a string array of each element, and creates a linkedlist ofo it going backwards again (since we reversed the order in the beginning, this sets it to the correct order but once added backwards)
     return createBackwardsLL(sumOfLL)
+// returns head linked list node
 };
