ad3cd3543381183ec7c516ea80a8aa501d43425d
gsanchez13 <gisellesanchez@pursuit.org>
2021-01-20 23:39:27 -0500

    creates sample LL for prob

--- leetcode/removeLLelements.js
+++ leetcode/removeLLelements.js
@@ -1,22 +1,40 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
  */
 /**
  * @param {ListNode} head
  * @param {number} val
  * @return {ListNode}
  */
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+let l1 = new ListNode(1)
+let l2 = new ListNode(2)
+let l3 = new ListNode(6)
+let l4 = new ListNode(3)
+let l5 = new ListNode(4)
+let l6 = new ListNode(5)
+let l7 = new ListNode(6)
+
+l1.next = l2
+l2.next = l3
+l3.next = l4
+l4.next = l5
+l5.next = l6
+l6.next = l7
+
+let num = 6
+
 var removeElements = function(head, val) {
     let answer = new ListNode(curr.val)
     let curr = head
     while(curr){
         if(curr.val != val){
            if(!answer){
-               answer = 
+               answer = new ListNode(curr.val)
                let answerCurr = answer
            }
             else {
@@ -27,3 +45,5 @@ var removeElements = function(head, val) {
         }
     } return answer
 };
+
+console.log(l1, num)
