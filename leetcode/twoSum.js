0a814f8bd89252d38ecce80b3173cd874f621186
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-06 08:10:49 -0400

    solves problem

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -9,13 +9,20 @@
 //  * @param {number} target
 //  * @return {number[]}
 
- var twoSum = function(nums, target) {
-
- };
+var twoSum = function (nums, target) {
+    result = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target && i !== j && result.length !== 2) {
+                result.push(i, j)
+            }
+        }
+    }
+    return result;
+};
  //1. create result variable to return at end
  //2. loop through array first time
- //3. set a variable to hold current space
- //4. loop through array second time
- //5. set condition that both numbers must equal the target, not be the same index & not have a result already
- //6. add numbers that successfully meet condition to result
- //7. outside of nested loop, return result variable
+ //3. loop through array second time
+ //4. set condition that both numbers must equal the target, not be the same index & not have a result already
+ //5. add numbers that successfully meet condition to result
+ //6. outside of nested loop, return result variable
