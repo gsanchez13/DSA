62b4a534d81ff90708012c98820246718d91b5e7
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-06 08:05:50 -0400

    creates step to resolve problem

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -10,4 +10,12 @@
 //  * @return {number[]}
 
  var twoSum = function(nums, target) {
- }
+
+ };
+ //1. create result variable to return at end
+ //2. loop through array first time
+ //3. set a variable to hold current space
+ //4. loop through array second time
+ //5. set condition that both numbers must equal the target, not be the same index & not have a result already
+ //6. add numbers that successfully meet condition to result
+ //7. outside of nested loop, return result variable
