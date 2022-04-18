7205f85a1828107e63f951e9392b26b8648c0290
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-17 23:58:19 -0400

    initiates closestsNums problem

--- coding_challenges/closestNums.js
+++ coding_challenges/closestNums.js
@@ -1,2 +1,4 @@
+// Given an array of distinctive integers, determine the minimum aboslute difference between any two elements. Print all element pairs with that minimal absolute difference in ascending order.
+
 const closestNums = (arr) => {
 };
