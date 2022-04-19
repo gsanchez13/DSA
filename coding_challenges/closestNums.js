64b33bdc9ca07b81d8135d7564fbca3081ae5ba5
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-18 23:28:31 -0400

    plans out first two steps

--- coding_challenges/closestNums.js
+++ coding_challenges/closestNums.js
@@ -1,6 +1,15 @@
 // Given an array of distinctive integers, determine the minimum aboslute difference between any two elements. Print all element pairs with that minimal absolute difference in ascending order.
 //Ex: numbers = [6,2,4,10]
 //The minimal absolute difference is 2 and the pairs with that difference are (2,4) and (4,6). When printing element pairs (i,j) they should be ordered acsending first by i and then j.
+// 2 4
+// 4 6
+//Function Description 
+//Complete the function closestNumers in the editor below. closestNumbers has the following parameter(s): int numbers[n]: an array of integers
+//Returns: NONE
+//Prints: distinct element pairs that share the minimal absolute difference, displayed in ascending order with each pair separated by one space on a single line.
+
+//1. Create variable to hold key value pairs and difference
+//2. Loop through array to find the minimum difference and build object
 
 const closestNums = (arr) => {
 };
