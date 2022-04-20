e44cdc76a4b9fd074735e426f21c4909463f725d
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-20 16:28:09 -0400

    initiates escapedCharacter problem

---
+++ coding_challenges/charactersEscaped.js
@@ -0,0 +1,12 @@
+//Given a string that consists of lowercase English letters and the characters '#' (ASCII value 35) and '!' (ASCII value 33), determine the number of times a character will be escaped. A character will be escaped if:
+// it is a lowercase english letter
+//there is a '!' character immediately before it and
+// both the lowercase english letter and the '!' character are between a starting and an ending '#' character
+//ex: '#ab!c#de!f'
+//The string isescaped once at '!c'. The substring '!f' is not between '#' characters so it is not escaped. Return 1.
+//Function Description
+//Complete the numberOfCharactersEscaped function in the editor below
+//numberOfCharactersEscaped function in that editor below
+
+const numberOfCharactersEscaped = () => {
+};
