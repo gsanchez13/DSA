b4e0365fb1d3a14a990c49dc08d1a9173d1aada4
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-20 23:17:39 -0400

    initiates firstRepeatedWord

---
+++ coding_challenges/firstRepeatedWord.js
@@ -0,0 +1,6 @@
+//A sentence is minimally defined as a word or a group of words that end with a period. A word is considered to be a sequence of letters delimited by a non-letter character. A repreated word is a case-sensitive word that appears more than once in a sentence (e.g. 'had' = 'had' but 'had' != 'Had'). Because substrings of a word are not delimited, they are not considered to be words, (e.g. 'hard' is not repeared in 'hardly'). Determine the first repeated word in a sentece.
+//ex: "We work hard because hard work pays."
+
+const firstRepeatedWord = (str) => {
+
+};
