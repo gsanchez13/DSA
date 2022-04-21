232edf53206ef32926c982a3f598d92c638727fa
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-20 22:49:40 -0400

    solves charactersEscaped

--- coding_challenges/charactersEscaped.js
+++ coding_challenges/charactersEscaped.js
@@ -3,10 +3,32 @@
 //there is a '!' character immediately before it and
 // both the lowercase english letter and the '!' character are between a starting and an ending '#' character
 //ex: '#ab!c#de!f'
-//The string isescaped once at '!c'. The substring '!f' is not between '#' characters so it is not escaped. Return 1.
+//The string is escaped once at '!c'. The substring '!f' is not between '#' characters so it is not escaped. Return 1.
 //Function Description
 //Complete the numberOfCharactersEscaped function in the editor below
 //numberOfCharactersEscaped function in that editor below
 
-const numberOfCharactersEscaped = () => {
+//1.loop through string
+//2. check if it meets conditions of being lowercase, having a ! before and a # after
+// let prev = str[i - 1];
+// let curr = str[i];
+// if ((/[a-z]/).test(curr) && prev === "!"){
+//     console.log(curr)
+// }
+const numberOfCharactersEscaped = (str) => {
+    let timesEscaped = 0;
+    let hashFinder = false;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "#") {
+            hashFinder = !hashFinder
+            continue
+        }
+        let next = str[i +1];
+        if ((/[a-z]/).test(next) && hashFinder && str[i] === "!") {
+            timesEscaped += 1
+        }
+    }
+    console.log(timesEscaped)
+    return timesEscaped
 };
+numberOfCharactersEscaped('#ab!c#de!f')
