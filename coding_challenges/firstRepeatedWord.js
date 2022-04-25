b97a534e7c33cc8c20c1182b6b4b41d7bac828aa
gsanchez13 <gisellesanchez@pursuit.org>
2022-04-24 23:05:29 -0400

    adds steps

--- coding_challenges/firstRepeatedWord.js
+++ coding_challenges/firstRepeatedWord.js
@@ -1,6 +1,10 @@
 //A sentence is minimally defined as a word or a group of words that end with a period. A word is considered to be a sequence of letters delimited by a non-letter character. A repreated word is a case-sensitive word that appears more than once in a sentence (e.g. 'had' = 'had' but 'had' != 'Had'). Because substrings of a word are not delimited, they are not considered to be words, (e.g. 'hard' is not repeared in 'hardly'). Determine the first repeated word in a sentece.
 //ex: "We work hard because hard work pays."
 
+//1. create object to hold key value pairs of repeated words - key is the word, value is static
+//2. convert input string into array
+//3. look through new array
+//4. apply conditions if word is not a key. create key. if word is already a key (repeated) & is not a symbol, return repeated word
 const firstRepeatedWord = (str) => {
     let repeatTracker = {};
     let strArr = str.split(' ');
