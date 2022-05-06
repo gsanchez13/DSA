fb97b20ae82382a206a2b4a737b8870b7d8b42ee
gsanchez13 <gisellesanchez@pursuit.org>
2022-05-06 09:00:43 -0400

    creates first solution with error in deleting key

--- leetcode/maxKsum.js
+++ leetcode/maxKsum.js
@@ -14,9 +14,40 @@
 // Explanation: Starting with nums = [3,1,3,4,3]:
 // - Remove the first two 3's, then nums = [1,4,3]
 // There are no more pairs that sum up to 6, hence a total of 1 operation.
-var maxOperations = function (nums, k) {
 
+//1.create return var with times operations can be performed
+//2. create obj to match values to see if they equal sum
+//3. loop through arr and if condition is met, splice out numbers in condition
+//4. add all times condition is met to return var
+
+var maxOperations = function (nums, k) {
+    let ops = 0;
+    let tracker = new Map();
+    if (nums.length === 0) return ops;
+    for (let i = 0; i < nums.length; i++) {
+        let curr = nums[i];
+        if (!tracker[i]) {
+            tracker[i] = curr
+        }
+    }
+    for (let key1 in tracker) {
+        let currVal = tracker[key1]
+        for (let key2 in tracker){
+            if(key1 !== key2){
+                let nextVal = tracker[key2]
+                if(currVal + nextVal === k){
+                    ops += 1;
+                    delete tracker[key1]
+                    //not properly deleting key1 here and is being reused
+                    delete tracker[key2]
+                }
+            }
+        }
+    }
+    return ops
 };
 
 let nums1 = [1, 2, 3, 4], k1 = 5;
 let nums2 = [3, 1, 3, 4, 3], k2 = 6;
+console.log(maxOperations(nums1, k1));
+console.log(maxOperations(nums2, k2));
