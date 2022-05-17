83623575f37c3992e6c90a9a0897a109e2f79cd9
gsanchez13 <gisellesanchez@pursuit.org>
2022-05-16 23:19:55 -0400

    adds test cases

--- leetcode/reverseInt.js
+++ leetcode/reverseInt.js
@@ -1,5 +1,17 @@
 // Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 // Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+// Input: x = 123
+// Output: 321
+
+// Input: x = -123
+// Output: -321
+
+// Input: x = 120
+// Output: 21
 var reverse = function(x) {
     
 };
+
+let test1 = 123
+let test2 = -321
+let test3 = 21
