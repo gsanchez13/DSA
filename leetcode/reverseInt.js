08cd9dea200dff08ac261fe4f098746137cb8f79
gsanchez13 <gisellesanchez@pursuit.org>
2022-05-17 23:54:50 -0400

    first solution for reverseInt

--- leetcode/reverseInt.js
+++ leetcode/reverseInt.js
@@ -8,9 +8,16 @@
 
 // Input: x = 120
 // Output: 21
-var reverse = function(x) {
-    
-};
+function reverse(n) {
+    var reverseN = 0;
+    var sign = n < 0;
+    n = Math.abs(n);
+    while (n) {
+        reverseN = reverseN*10 + (n % 10);
+        n = Math.floor(n/10);
+    }
+    return reverseN > 0x7FFFFFFF ? 0 : sign ? -reverseN : reverseN;
+}
 
 let test1 = 123
 let test2 = -321
