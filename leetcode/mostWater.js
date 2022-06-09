f9bd29f3b8975e05e45a3ce9e2fba4f143fd885f
gsanchez13 <gisellesanchez@pursuit.org>
2022-06-08 23:56:48 -0400

    adds comments

--- leetcode/mostWater.js
+++ leetcode/mostWater.js
@@ -5,24 +5,24 @@
 // Return the maximum amount of water a container can store.
 
 // Notice that you may not slant the container.
-var maxArea = function (height) {
-    let tracker = {};
-    let max = 0;
-    for (let i = 0, w = 1; i < height.length; i++, w++) {
-        let curr = height[i];
-        for (let j = 0; j < height.length; j++) {
-            let next = height[j]
-            if(j !== i){
-                if(next <= curr){
-                    max = (curr * w)
-                    console.log(max)
-                }
-            }
-        }
-    }
-    console.log(tracker)
-    return max
-};
+// var maxArea = function (height) {
+//     let tracker = {};
+//     let max = 0;
+//     for (let i = 0, w = 1; i < height.length; i++, w++) {
+//         let curr = height[i];
+//         for (let j = 0; j < height.length; j++) {
+//             let next = height[j]
+//             if(j !== i){
+//                 if(next <= curr){
+//                     max = (curr * w)
+//                     console.log(max)
+//                 }
+//             }
+//         }
+//     }
+//     console.log(tracker)
+//     return max
+// };
 // Input: height = [1,8,6,2,5,4,8,3,7]
 // Output: 49
 // Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
@@ -31,18 +31,19 @@ var maxArea = function (height) {
 let test1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 // maxArea(test1)
 
-var maxArea2 = function(height) {
+var maxArea2 = function (height) {
     let n = height.length;
-    let left= 0, area= 0;
-    let right= n-1;
-    
-    while(left<right){
-        if(height[left]<= height[right])
-        {  area= Math.max(area, height[left]*(right-left));
+    let left = 0, area = 0;
+    let right = n - 1;
+
+    while (left < right) {
+        if (height[left] <= height[right]) {
+            area = Math.max(area, height[left] * (right - left));
             left++;
+            //uses while loop and tree to determine max area
         }
-        else
-        {  area= Math.max(area, height[right]*(right-left));
+        else {
+            area = Math.max(area, height[right] * (right - left));
             right--;
         }
     }
