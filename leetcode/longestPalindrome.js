bd08de1c655a9d3ace595aab41d13cf7f0213fac
gsanchez13 <gisellesanchez@pursuit.org>
2022-06-12 23:51:59 -0400

    adds backwards loop

--- leetcode/longestPalindrome.js
+++ leetcode/longestPalindrome.js
@@ -8,7 +8,17 @@
 var longestPalindrome = function(s) {
     let ans = 0 ;
     let tracker = "";
+    let tracker2 = "";
     for(let i = 0; i < s.length; i++){
         tracker += s[i]
+        for(let j = tracker.length; j >= 0; j--){
+            tracker2 += tracker[j]
+        }
+        if(tracker2 === tracker){
+            if(tracker2.length > tracker.length){
+                ans = tracker2.length
+            }
+        }
     }
+    return ans
 };
